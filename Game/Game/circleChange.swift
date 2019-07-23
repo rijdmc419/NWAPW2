9bde62fecf0982b2fb0c55b34e1aea64e3827291
Rij Dorfman <rdorfman419@outlook.com>
2019-07-23 12:53:24 -0700

    5 circles, 2 squares

--- Game/Game/circleChange.swift
+++ Game/Game/circleChange.swift
@@ -43,10 +43,19 @@ class circleChange: SKScene {
 
     var arraySprites : [Shape]!
     var circ_made1: SKSpriteNode!
+    var circ_made2: SKSpriteNode!
+    var circ_made3: SKSpriteNode!
+    var circ_made4: SKSpriteNode!
+    var circ_made5: SKSpriteNode!
     var circ1 : Circle!
+    var circ2 : Circle!
+    var circ3 : Circle!
+    var circ4 : Circle!
+    var circ5 : Circle!
     var square_made1: SKSpriteNode!
+    var square_made2: SKSpriteNode!
     var square1 : Square!
-    var regualr_guy: SKNode!
+    //var regualr_guy: SKNode!
      //var sprite_array: [SKSpriteNode] = []
     
     
@@ -68,10 +77,9 @@ class circleChange: SKScene {
         circ_made1 = SKSpriteNode(imageNamed: "Circle_White")
         circ_made1.name = "circ_made1"
         circ_made1.size = CGSize (width: 100, height: 100)
-        circ_made1.position = CGPoint (x: 0, y: 200)
+        circ_made1.position = CGPoint (x: 0, y: -150)
         circ_made1.anchorPoint = CGPoint (x: 0.5, y: 0.5)
-        self.addChild(circ_made1)
-        
+                    self.addChild(circ_made1)
         circ_made1.physicsBody = SKPhysicsBody(circleOfRadius: 50)
         circ_made1.physicsBody?.isDynamic = true
         circ_made1.physicsBody?.affectedByGravity = false
@@ -80,21 +88,87 @@ class circleChange: SKScene {
         circ_made1.physicsBody?.angularDamping = 0.0
         circ_made1.physicsBody?.restitution = 1.0
         circ_made1.physicsBody?.friction = 0
-        
         circ_made1.physicsBody?.applyImpulse(CGVector(dx: 90, dy: 90))
-
+        circ1 = Circle(shape_sprite: circ_made1, isPink: false)
+        
+        circ_made2 = SKSpriteNode(imageNamed: "Circle_White")
+        circ_made2.name = "circ_made2"
+        circ_made2.size = CGSize (width: 100, height: 100)
+        circ_made2.position = CGPoint (x: 0, y: 30)
+        circ_made2.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+                   self.addChild(circ_made2)
+        circ_made2.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        circ_made2.physicsBody?.isDynamic = true
+        circ_made2.physicsBody?.affectedByGravity = false
+        circ_made2.physicsBody?.allowsRotation = false
+        circ_made2.physicsBody?.linearDamping = 0.0
+        circ_made2.physicsBody?.angularDamping = 0.0
+        circ_made2.physicsBody?.restitution = 1.0
+        circ_made2.physicsBody?.friction = 0
+        circ_made2.physicsBody?.applyImpulse(CGVector(dx: 90, dy: 90))
+        circ2 = Circle(shape_sprite: circ_made2, isPink: false)
+        
+        circ_made3 = SKSpriteNode(imageNamed: "Circle_White")
+        circ_made3.name = "circ_made3"
+        circ_made3.size = CGSize (width: 100, height: 100)
+        circ_made3.position = CGPoint (x: 0, y: 400)
+        circ_made3.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+                    self.addChild(circ_made3)
+        circ_made3.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        circ_made3.physicsBody?.isDynamic = true
+        circ_made3.physicsBody?.affectedByGravity = false
+        circ_made3.physicsBody?.allowsRotation = false
+        circ_made3.physicsBody?.linearDamping = 0.0
+        circ_made3.physicsBody?.angularDamping = 0.0
+        circ_made3.physicsBody?.restitution = 1.0
+        circ_made3.physicsBody?.friction = 0
+        circ_made3.physicsBody?.applyImpulse(CGVector(dx: 90, dy: 90))
+        circ3 = Circle(shape_sprite: circ_made3, isPink: false)
+        
+        circ_made4 = SKSpriteNode(imageNamed: "Circle_White")
+        circ_made4.name = "circ_made4"
+        circ_made4.size = CGSize (width: 100, height: 100)
+        circ_made4.position = CGPoint (x: 0, y: 200)
+        circ_made4.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+        self.addChild(circ_made4)
+        circ_made4.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        circ_made4.physicsBody?.isDynamic = true
+        circ_made4.physicsBody?.affectedByGravity = false
+        circ_made4.physicsBody?.allowsRotation = false
+        circ_made4.physicsBody?.linearDamping = 0.0
+        circ_made4.physicsBody?.angularDamping = 0.0
+        circ_made4.physicsBody?.restitution = 1.0
+        circ_made4.physicsBody?.friction = 0
+        circ_made4.physicsBody?.applyImpulse(CGVector(dx: 90, dy: 90))
+        circ4 = Circle(shape_sprite: circ_made4, isPink: false)
+        
+        circ_made5 = SKSpriteNode(imageNamed: "Circle_White")
+        circ_made5.name = "circ_made5"
+        circ_made5.size = CGSize (width: 100, height: 100)
+        circ_made5.position = CGPoint (x: 0, y: 100)
+        circ_made5.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+        self.addChild(circ_made5)
+        circ_made5.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        circ_made5.physicsBody?.isDynamic = true
+        circ_made5.physicsBody?.affectedByGravity = false
+        circ_made5.physicsBody?.allowsRotation = false
+        circ_made5.physicsBody?.linearDamping = 0.0
+        circ_made5.physicsBody?.angularDamping = 0.0
+        circ_made5.physicsBody?.restitution = 1.0
+        circ_made5.physicsBody?.friction = 0
+        circ_made5.physicsBody?.applyImpulse(CGVector(dx: 90, dy: 90))
+        circ5 = Circle(shape_sprite: circ_made5, isPink: false)
         
         //regualr_guy.addChild(circ_made1)
         
-        circ1 = Circle(shape_sprite: circ_made1, isPink: false)
+        
         
         square_made1 = SKSpriteNode(imageNamed: "square_white")
         square_made1.name = "square_made1"
         square_made1.size = CGSize (width: 100, height: 100)
         square_made1.position = CGPoint (x: 0, y: 0)
         square_made1.anchorPoint = CGPoint (x: 0.5, y: 0.5)
-        self.addChild(square_made1)
-        
+                    self.addChild(square_made1)
         square_made1.physicsBody = SKPhysicsBody(rectangleOf: CGSize (width: 100,height: 100))
         square_made1.physicsBody?.isDynamic = true
         square_made1.physicsBody?.affectedByGravity = false
@@ -103,9 +177,23 @@ class circleChange: SKScene {
         square_made1.physicsBody?.angularDamping = 0.0
         square_made1.physicsBody?.restitution = 1.0
         square_made1.physicsBody?.friction = 0
-        
         square_made1.physicsBody?.applyImpulse(CGVector(dx: 90, dy: 90))
 
+        square_made2 = SKSpriteNode(imageNamed: "square_white")
+        square_made2.name = "square_made2"
+        square_made2.size = CGSize (width: 100, height: 100)
+        square_made2.position = CGPoint (x: 0, y: 0)
+        square_made2.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+                   self.addChild(square_made2)
+        square_made2.physicsBody = SKPhysicsBody(rectangleOf: CGSize (width: 100,height: 100))
+        square_made2.physicsBody?.isDynamic = true
+        square_made2.physicsBody?.affectedByGravity = false
+        square_made2.physicsBody?.allowsRotation = false
+        square_made2.physicsBody?.linearDamping = 0.0
+        square_made2.physicsBody?.angularDamping = 0.0
+        square_made2.physicsBody?.restitution = 1.0
+        square_made2.physicsBody?.friction = 0
+        square_made2.physicsBody?.applyImpulse(CGVector(dx: 90, dy: 90))
         
         //arraySprites.append(circ)
     }
@@ -128,9 +216,55 @@ class circleChange: SKScene {
                 print("part3")
             }
             
+            if clickedNodes.first?.name == "circ_made2" {
+                if circ2.isPink == false {
+                    print("part2")
+                    changetoPink(circle_shape: circ_made2)
+                    circ2.isPink = true
+                }
+                
+                print("part3")
+            }
+            
+            if clickedNodes.first?.name == "circ_made3" {
+                if circ3.isPink == false {
+                    print("part2")
+                    changetoPink(circle_shape: circ_made3)
+                    circ3.isPink = true
+                }
+                
+                print("part3")
+            }
+            
+            if clickedNodes.first?.name == "circ_made4" {
+                if circ4.isPink == false {
+                    print("part2")
+                    changetoPink(circle_shape: circ_made4)
+                    circ4.isPink = true
+                }
+                
+                print("part3")
+            }
+            
+            if clickedNodes.first?.name == "circ_made5" {
+                if circ5.isPink == false {
+                    print("part2")
+                    changetoPink(circle_shape: circ_made5)
+                    circ5.isPink = true
+                }
+                
+                print("part3")
+            }
+            
             if clickedNodes.first?.name == "square_made1" {
                 square_made1.alpha = 0
+                square_made1.physicsBody = SKPhysicsBody(rectangleOf: CGSize (width: 00,height: 00))
                 
+            }
+            
+            if clickedNodes.first?.name == "square_made2" {
+                square_made2.alpha = 0
+                square_made2.physicsBody = SKPhysicsBody(rectangleOf: CGSize (width: 00,height: 00))
                 
             }
             
@@ -148,6 +282,25 @@ class circleChange: SKScene {
             circle_shape.texture = SKTexture(imageNamed: "Circle_Pink")
             print("Hi")
     }
+    /*
+    func makeCircSprite() {
+        
+        circ_made1 = SKSpriteNode(imageNamed: "Circle_White")
+        circ_made1.name = "circ_made1"
+        circ_made1.size = CGSize (width: 100, height: 100)
+        circ_made1.position = CGPoint (x: 0, y: 200)
+        circ_made1.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+        self.addChild(circ_made1)
+        circ_made1.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        circ_made1.physicsBody?.isDynamic = true
+        circ_made1.physicsBody?.affectedByGravity = false
+        circ_made1.physicsBody?.allowsRotation = false
+        circ_made1.physicsBody?.linearDamping = 0.0
+        circ_made1.physicsBody?.angularDamping = 0.0
+        circ_made1.physicsBody?.restitution = 1.0
+        circ_made1.physicsBody?.friction = 0
+    }
+ */
 }
 
 
