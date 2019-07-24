a03ada0a724373ce32fe73e30464b869b0bd5bcc
Amelia Ellis <aspam300@gmail.com>
2019-07-24 14:45:55 -0700

    aaa im angered

--- Game/Game/Level1.swift
+++ Game/Game/Level1.swift
@@ -10,6 +10,7 @@ import SpriteKit
 import GameplayKit
 import UIKit
 
+
 class Level1: SKScene, SKPhysicsContactDelegate
 {
     var good1: SKSpriteNode!
@@ -18,8 +19,14 @@ class Level1: SKScene, SKPhysicsContactDelegate
     var good2: SKSpriteNode!
     var bad2: SKSpriteNode!
     
+    var lineb: SKSpriteNode!
+    var linet: SKSpriteNode!
+    var liner: SKSpriteNode!
+    var linel: SKSpriteNode!
+    
     let goodCatergory:UInt32 = 0x1 << 0 //1
     let badCategorry:UInt32 = 0x1 << 2 //4
+    let wallCategoy:UInt32 = 0x1 << 3 // 6?
     var isPink: Bool = false
 
     override func didMove(to view: SKView) {
@@ -32,6 +39,60 @@ class Level1: SKScene, SKPhysicsContactDelegate
         boarder.friction = 0
         boarder.restitution = 1.0
         self.physicsBody = boarder
+        
+        //bottom boarder
+        lineb = SKSpriteNode(imageNamed: "line2")
+        lineb.size = CGSize (width: 500, height: 2)
+        lineb.position = CGPoint (x: 0, y: -250)
+        lineb.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+        self.addChild(lineb)
+        lineb.physicsBody = SKPhysicsBody(rectangleOf:  CGSize (width: 500, height: 2))
+        lineb.physicsBody?.isDynamic = false
+        lineb.physicsBody?.friction = 0
+        lineb.physicsBody?.restitution = 1
+        lineb.name = "wall"
+        
+        //topboarder
+        linet = SKSpriteNode(imageNamed: "line2")
+        linet.size = CGSize (width: 500, height: 2)
+        linet.position = CGPoint (x: 0, y: 250)
+        linet.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+        self.addChild(linet)
+        linet.physicsBody = SKPhysicsBody(rectangleOf:  CGSize (width: 500, height: 2))
+        linet.physicsBody?.isDynamic = false
+        linet.physicsBody?.friction = 0
+        linet.physicsBody?.restitution = 1
+        linet.name = "wall"
+        
+        
+        
+        //rightside baorder
+        liner = SKSpriteNode(imageNamed: "line2")
+        liner.size = CGSize (width: 20, height: 500)
+        liner.position = CGPoint (x: 250, y: 0)
+        liner.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+        liner.physicsBody = SKPhysicsBody(rectangleOf:  CGSize (width: 20, height: 500))
+        liner.physicsBody?.isDynamic = false
+        liner.physicsBody?.friction = 1
+        liner.physicsBody?.restitution = 1
+        liner.physicsBody?.affectedByGravity = false
+        self.addChild(liner)
+        liner.name = "wall"
+
+        
+        //leftside boarder
+        linel = SKSpriteNode(imageNamed: "line2")
+        linel.size = CGSize (width: 2, height: 500)
+        linel.position = CGPoint (x: -250, y: 0)
+        linel.anchorPoint = CGPoint (x: 0.5, y: 0.5)
+        self.addChild(linel)
+        linel.physicsBody = SKPhysicsBody(rectangleOf:  CGSize (width: 2, height: 500))
+        linel.physicsBody?.isDynamic = false
+        linel.physicsBody?.friction = 0
+        linel.physicsBody?.restitution = 1
+        linel.name = "wall"
+
+
         
        //Good One
         good1 = SKSpriteNode(imageNamed: "Circle_White")
@@ -85,6 +146,7 @@ class Level1: SKScene, SKPhysicsContactDelegate
         bad1.physicsBody?.angularDamping = 0.0
         bad1.physicsBody?.restitution = 1.0
         bad1.physicsBody?.friction = 0
+        bad1.physicsBody?.mass = 1
         
         //Bad2
         bad2 = SKSpriteNode(imageNamed: "squareish")
@@ -102,19 +164,28 @@ class Level1: SKScene, SKPhysicsContactDelegate
         bad2.physicsBody?.angularDamping = 0.0
         bad2.physicsBody?.restitution = 1.0
         bad2.physicsBody?.friction = 0
-        
+        bad2.physicsBody?.mass = 1
         //Apply movement
         good1.physicsBody?.applyImpulse(CGVector(dx: 15, dy: 15))
-        bad1.physicsBody?.applyImpulse(CGVector(dx: -15, dy: -15))
+        bad1.physicsBody?.applyImpulse(CGVector(dx: -5, dy: -5))
         good2.physicsBody?.applyImpulse(CGVector(dx: 15, dy: -15))
-        bad2.physicsBody?.applyImpulse(CGVector(dx: 15, dy: 15))
+        bad2.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 5))
         
         // two
         bad1.physicsBody?.categoryBitMask = badCategorry
         good1.physicsBody?.collisionBitMask = goodCatergory
         bad1.physicsBody?.collisionBitMask = goodCatergory
         good1.physicsBody?.categoryBitMask = goodCatergory
-
+        
+        /*lineb.physicsBody?.categoryBitMask = wallCategoy
+        lineb.physicsBody?.collisionBitMask = goodCatergory
+        linet.physicsBody?.categoryBitMask = wallCategoy
+        linet.physicsBody?.collisionBitMask = goodCatergory
+        liner.physicsBody?.categoryBitMask = wallCategoy
+        liner.physicsBody?.collisionBitMask = goodCatergory
+        linel.physicsBody?.categoryBitMask = wallCategoy
+        linel.physicsBody?.collisionBitMask = goodCatergory*/
+        
         bad1.physicsBody?.contactTestBitMask = goodCatergory
         
         //four
@@ -131,22 +202,51 @@ class Level1: SKScene, SKPhysicsContactDelegate
     {
         let firstBody = contact.bodyA.node as! SKSpriteNode
         let secondBody = contact.bodyB.node as! SKSpriteNode
+        
+        
     
-        if ((firstBody.name == "good") && (secondBody.name == "bad"))
-        {
-            collisions(good: firstBody, bad: secondBody)
-        }
-        else if ((firstBody.name == "bad") && (secondBody.name == "good"))
+        if ((firstBody.name == "bad") && (secondBody.name == "good"))
         {
             collisions(good: secondBody, bad: firstBody)
         }
+        else if ((firstBody.name == "good") && (secondBody.name == "bad"))
+        {
+            collisions(good: firstBody, bad: secondBody)
+        }
+        /*else if ((firstBody.name == "bad") && (secondBody.name == "wall"))
+        {
+            //boarderCollisions(boarders: firstBody, shape: secondBody)
+            print("aaa")
+        }
+        else if ((firstBody.name == "wall") && (secondBody.name == "bad"))
+        {
+            //boarderCollisions(boarders: secondBody, shape: firstBody)
+            print("aaa")
+
+        }
+        else if ((firstBody.name == "wall") && (secondBody.name == "good"))
+        {
+            //boarderCollisions(boarders: secondBody, shape: firstBody)
+            print("aaa")
+
+        }
+        else if ((firstBody.name == "good") && (secondBody.name == "wall"))
+        {
+            //boarderCollisions(boarders: firstBody, shape: secondBody)
+            print("aaa")
+        }*/
 
     }
     
     func collisions(good : SKSpriteNode, bad : SKSpriteNode)
     {
-        good.size = CGSize (width: 100, height: 100)
+        good.texture = SKTexture(imageNamed: "Circle_Pink")
     }
+    
+    /*func boarderCollisions(boarders : SKSpriteNode, shape : SKSpriteNode)
+    {
+         print("boarder collisions")
+    }*/
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             }
